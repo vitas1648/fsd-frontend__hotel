f3b4a5bd11682d64b150b33b79bbfb9728704549
Viktor Chegodaev <vitas1648@gmail.com>
2019-05-11 12:56:44 +0500

    Added building CSS from SCSS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -11,12 +12,19 @@ module.exports = {
         rules: [{
             test: /\.pug$/,
             use: ["pug-loader"]
-        }, ]
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.pug'
         }),
+        new ExtractTextPlugin('style.css')
     ]
 };
