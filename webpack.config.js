9f1859d14049ca6bc9d60a7a953bcc3fdbdef44f
Viktor Chegodaev <vitas1648@gmail.com>
2019-05-11 12:39:11 +0500

    Added building HTML from PUG.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.pug$/,
+            use: ["pug-loader"]
+        }, ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.pug'
+        }),
+    ]
 };
